629e8fd4cf0cb13bf7a72e55d896d12f38c87985
MrPelmenis <misterpelmenis@gmail.com>
2024-02-28 16:03:36 +0200

    tailwind setup esmy kinda iemacijies
    sakt taisim

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <section className="h-screen w-screen bg-slate-600 flex justify-center items-center">
+          <span className='bg-orange-500 h-1/2 w-3/4 flex items-center justify-center rounded-xl'>
+            
+          </span>
+        </section>
+    )
+}
+
+export default App;
