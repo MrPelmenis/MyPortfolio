31f28142c28b0922ab3523c22af9c44cc9fc3ad9
MrPelmenis <misterpelmenis@gmail.com>
2024-02-29 18:35:40 +0200

    paraadaas
    turpinat

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
+import TopBar from "./TopBar/TopBar";
 
 function App() {
-    return (
+
+  useEffect(()=>{
+  });
+
+  return (
         <section className="h-screen w-screen bg-slate-600 flex justify-center items-center">
-          <span className='bg-orange-500 h-1/2 w-3/4 flex items-center justify-center rounded-xl'>
-            
+          <span className='bg-orange-500 h-1/2 w-3/4 rounded-xl slideIn'>
+              <TopBar></TopBar>
           </span>
         </section>
-    )
+  )
 }
 
 export default App;
---
+++ my-portfolio/src/TopBar/TopBar.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+function TopBar() {
+
+  useEffect(()=>{
+  });
+
+  return (
+    <div className='width-full border border-black h-40px rounded' >aaa</div>
+  )
+}
+
+export default TopBar;
