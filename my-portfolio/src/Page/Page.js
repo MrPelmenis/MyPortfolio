b0185fa0017cb3b12234e070eb9aa93e0934514b
MrPelmenis <46497841+MrPelmenis@users.noreply.github.com>
2024-03-11 11:13:46 +0200

    daudz bilzu adotas
    yeye

--- my-portfolio/src/Page/Page.js
+++ my-portfolio/src/Page/Page.js
@@ -3,11 +3,39 @@ import './Page.css';
 
 
 import homeImage from "../img/image.png";
+
 import phpLogo from "../img/phpLogo.png";
+import jsLogo from "../img/jsLogo.png";
+import htmlLogo from "../img/htmlLogo.png";
+import cssLogo from "../img/cssLogo.png";
+import mysqlLogo from "../img/mysqlLogo.png";
+import reactLogo from "../img/reactLogo.png";
+import nodeJsLogo from "../img/nodeJsLogo.png";
+import javaLogo from "../img/javaLogo.jpg";
+import csharpLogo from "../img/csharpLogo.png";
+import cPlusPlusLogo from "../img/c++Logo.png";
+import tailwindLogo from "../img/tailwindLogo.png";
+
+
+
 import boidsImg from "../img/boids.png";
+import naggersImg from "../img/naggers.png";
+import beatMakerImg from "../img/beatMaker.png";
+import asteroidGameImg from "../img/asteroidGame.png";
+import epicKillingGame from "../img/crimsonland.png";
+import tankGame from "../img/tankGame.png";
+import threeDterrain from "../img/3dterrain.png"
+import game2048 from "../img/2048.png";
+import gameofLife from "../img/game of life.png";
+import bird from "../img/bird.png";
+import imgtoTxt from "../img/imgtotext.png";
+import maze from "../img/3dmaze.png";
 
 import { MdEmail } from "react-icons/md";
 import { MdLocalPhone } from "react-icons/md";
+import { MdFacebook } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 function Page() {
     const containerRef = useRef(null);
@@ -31,32 +59,88 @@ function Page() {
 
 
 function HomePage() {
-  const [isHovered, setIsHovered] = useState(false);
+ 
   return (
-    <section id='home' className='w-full h-full flex justify-between overflow-hidden flex-shrink-0 items-center'>
-      <img className='mx-6 h-48 md:h-64 lg:h-72 xl:h-80 md:w-50 lg:w-72 xl:w-80 sm:w-30 shake' src={homeImage} alt="Home Image" />
-      <div className='mx-6 md:text-lg lg:text-xl xl:text-2xl max-w-full md:max-w-3/4 lg:max-w-2/3 xl:max-w-1/2 h-full overflow-y-auto' style={{paddingBottom:"60px"}}>
+    <section id='home' className='w-full h-full flex justify-between overflow-hidden flex-shrink-0 flex-col items-center sm:flex-col md:flex-col lg:flex-row xl:flex-row'>
+      <img className='mx-6 h-48 md:h-auto lg:h-auto xl:h-auto shake' src={homeImage} alt="Home Image" />
+      <div className='mx-6 md:text-lg lg:text-xl xl:text-2xl max-w-full md:max-w-3/4 lg:max-w-2/3 xl:max-w-1/2 h-full overflow-y-auto flex flex-col justify-center items-center' style={{paddingBottom:"90px", padding:"20px"}}>
         Mind what no by kept. Celebrated no he decisively thoroughly. Our asked sex point her she seems. New plenty she horses parish design you. Stuff sight equal of my woody. Him children bringing goodness suitable she entirely put far daughter.
         Received shutters expenses ye he pleasant. Drift as blind above at up. No up simple county stairs do should praise as. Drawings sir gay together landlord had law smallest. Formerly welcomed attended declared met say unlocked. Jennings outlived no dwelling denoting in peculiar as he believed. Behaviour excellent middleton be as it curiosity departure ourselves.
-       
-          <span
-            className={`flex text-center flex-col items-center mx-2 mt-5 border border-black rounded-lg bg-my-cyan overflow-hidden projectShowcase transition-all duration-300 ${isHovered ? 'hover:scale-110' : ''}`}
-            style={{width: isHovered ? '300px' : '150px', height: isHovered ? '100px' : '30px'}}
+        <div className='flex flex-wrap justify-center'>
+          <ContactMe></ContactMe>
+          <GithubDisplay></GithubDisplay>  
+          <Socials></Socials>
+        </div>
+      </div>
+  </section>
+  );
+}
+
+
+function ContactMe(){
+  const [isHovered, setIsHovered] = useState(false);
+  return(
+  <span
+      className={`flex text-center flex-col items-center mx-3 mt-5 border border-black rounded-lg bg-my-cyan overflow-hidden projectShowcase transition-all duration-300 ${isHovered ? 'hover:scale-110' : ''}`}
+            style={{width: isHovered ? '260px' : '150px', height: isHovered ? '90px' : '30px'}}
             onMouseEnter={() => {
                 setIsHovered(true);
             }} 
             onMouseLeave={() => setIsHovered(false)}>
 
-            <h1 className='font-bold select-none'>Contact Me</h1>
+            <h1 className='select-none'>Contact Me</h1>
             <span className="slowAppearingText text-base">
-                <div className='flex justify-center items-center'><MdLocalPhone/>25299736</div>
-                <div className='flex justify-center items-center'><MdEmail/> normundsmalnacs@gmail.com</div>
-            </span>
-        </span>
-      </div>
-  </section>
-  );
+                <div className='flex justify-center items-center'><MdLocalPhone className='mx-1'/>25299736</div>
+                <div className='flex justify-center items-center'><MdEmail className='mx-1'/> normundsmalnacs@gmail.com</div>
+            </span> 
+  </span>
+  )
 }
+
+function Socials(){
+  const [isHovered, setIsHovered] = useState(false);
+  return(
+  <span
+      className={`flex text-center flex-col items-center mx-3 mt-5 border border-black rounded-lg bg-orange-400 overflow-hidden projectShowcase transition-all duration-300 ${isHovered ? 'hover:scale-110' : ''}`}
+            style={{width: isHovered ? '260px' : '150px', height: isHovered ? '90px' : '30px'}}
+            onMouseEnter={() => {
+                setIsHovered(true);
+            }} 
+            onMouseLeave={() => setIsHovered(false)}>
+
+            <h1 className='select-none'>Socials</h1>
+            <span className="slowAppearingText text-base">
+                <div className='flex justify-center items-center'><FaInstagram className='mx-1'></FaInstagram><a href='https://www.instagram.com/normuns/' className='underlineOnHover'>normuns</a></div>
+                <div className='flex justify-center items-center'><MdFacebook className='mx-1'></MdFacebook> <a href='https://www.facebook.com/normis.malcs/?locale=lv_LV' className='underlineOnHover'>Normunds Malnačs</a></div>
+            </span> 
+  </span>
+  )
+}
+
+
+function GithubDisplay(){
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  
+  return (
+    <span
+      className={`flex text-center flex-col items-center mx-3 mt-5 border border-black rounded-lg bg-purple-600 overflow-hidden projectShowcase transition-all duration-300 ${isHovered || isClicked ? 'hover:scale-110' : ''} ${isClicked ? 'clicked' : ''}`}
+      style={{width: isHovered || isClicked ? '200px' : '150px', height: isHovered || isClicked ? '60px' : '30px'}}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseDown={() => setIsClicked(!isClicked)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <h1 className='select-none' style={{color:"white"}}>Github</h1>
+      <span className="slowAppearingText text-base">
+        <div className='flex justify-center items-center underlineOnHover' style={{color:"white"}}>
+          <a href='https://github.com/MrPelmenis' className='font-bold flex justify-center items-center'>
+            <FaGithub className='mx-1'/> Check out my work
+          </a>
+        </div>
+      </span> 
+    </span>
+  );
+ }
 
 
 function AboutPage() {
@@ -67,9 +151,19 @@ function AboutPage() {
       <h1 className='font-bold mx-3 md:text-xl lg:text-2xl xl:text-3xl mt-5'>IT Knowledge:</h1>
       <div className='mx-3 md:text-lg lg:text-xl xl:text-2xl mb-1'>Fujak es ahujennais it fujaks</div>
       <div className='turuWidgetus mb-20'>
+        <LanguageShowcase img={jsLogo} text="Js"></LanguageShowcase>
+        <LanguageShowcase img={htmlLogo} text="Html"></LanguageShowcase>
+        <LanguageShowcase img={cssLogo} text="Css"></LanguageShowcase>
+        <LanguageShowcase img={reactLogo} text="React"></LanguageShowcase>
+        <LanguageShowcase img={tailwindLogo} text="Tailwind"></LanguageShowcase>
+        <br/>
+        <LanguageShowcase img={nodeJsLogo} text="Node Js"></LanguageShowcase>
         <LanguageShowcase img={phpLogo} text="php"></LanguageShowcase>
-        <LanguageShowcase img={phpLogo} text="php"></LanguageShowcase>
-        <LanguageShowcase img={phpLogo} text="php"></LanguageShowcase>
+        <LanguageShowcase img={mysqlLogo} text="mysql"></LanguageShowcase>
+        <br/>
+        <LanguageShowcase img={csharpLogo} text="C#"></LanguageShowcase>
+        <LanguageShowcase img={cPlusPlusLogo} text="C++"></LanguageShowcase>
+        <LanguageShowcase img={javaLogo} text="Java"></LanguageShowcase>
       </div>
    </section>
   );
@@ -77,22 +171,27 @@ function AboutPage() {
 
 function ProjectsPage(){
   return (
-    <section id='projects' className='w-full h-full flex-shrink-0' >
+    <section id='projects' className='w-full h-full flex-shrink-0 flex flex-col' >
       
       <h1 className='font-bold mt-5 ml-5 md:text-lg lg:text-xl xl:text-2xl'>My personal projects:</h1>
       <section className='flex flex-wrap overflow-y-auto h-full justify-center mt-3' style={{paddingBottom:"60px"}}>
+        <ProjectShowcase img={naggersImg} title={"Full Stack Website"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        
+        <ProjectShowcase img={beatMakerImg} title={"Beat Maker Web"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+
+        <ProjectShowcase img={asteroidGameImg} title={"Retro Asteroid Game"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        <ProjectShowcase img={epicKillingGame} title={"Crimsonland Alternative"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        <ProjectShowcase img={tankGame} title={"Online Multiplayer Game"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        <ProjectShowcase img={bird} title={"Flappy Bird Remake"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+
+        <ProjectShowcase img={game2048} title={"2048 Alternative"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        <ProjectShowcase img={gameofLife} title={"Conway's Game Of Life"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        
+        <ProjectShowcase img={imgtoTxt} title={"Image To Text Converter"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+
+        <ProjectShowcase img={threeDterrain} title={"3D Terrain Generation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
+        <ProjectShowcase img={maze} title={"3D Maze Generation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
         <ProjectShowcase img={boidsImg} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
-        <ProjectShowcase img={phpLogo} title={"3D Boids Simulation"} text={"Nu kko jau es te uztaisiju, bija kinda gruti bet beigaas sagaaja viss kaa gribeju"}></ProjectShowcase>
         <div className='w-full' style={{height:100}}></div>
       </section>
       
@@ -107,13 +206,13 @@ function ProjectShowcase(props){
     return(
         <span 
             className={`flex flex-col items-center text-center  mx-2 mt-5 border-black overflow-hidden projectShowcase ${isHovered ? 'hover:scale-110' : ''}`} 
-            style={{width: isHovered ? '200px' : '200px', height: isHovered ? '200px' : '150px', marginBottom: isHovered ? "10px": "0"}}
+            style={{width: isHovered ? '250px' : '200px', height: isHovered ? '200px' : '150px', marginBottom: isHovered ? "10px": "0"}}
             onMouseEnter={() => {
               setIsHovered(true);
             }} 
             onMouseLeave={() => setIsHovered(false)}>
 
-              <img src={props.img} className="mx-auto mt-0" style={{width:100, height:100}} alt="Project Image" />
+              <img src={props.img} className="mx-auto mt-0 projectImage"  alt="Project Image" />
               <h1 className='font-bold'>{props.title}</h1>
               <span className={`slowAppearingText`}>{props.text}</span>
         </span>
@@ -123,9 +222,9 @@ function ProjectShowcase(props){
 
 function LanguageShowcase(props){
   return (
-    <span className='ml-2 inline-flex rounded-xl bord border select-none border-black items-center justify-between languageWidget'>
-      <img src={props.img} className='ml-2 rounded-xl' style={{width:25, height:25}} alt="Language Icon" />
-      <span className='mr-2 ml-1 font-arial'>{props.text}</span>
+    <span className='ml-2 inline-flex rounded-full bord border select-none border-black items-center justify-between languageWidget'>
+      <img src={props.img} className='ml-2 rounded-full' style={{width:30, height:30}} alt="Language Icon" />
+      <span className='mr-2 ml-1 font-arial font-bold'>{props.text}</span>
     </span>
   )
 }
--- my-portfolio/src/TopBar/TopBar.js
+++ my-portfolio/src/TopBar/TopBar.js
@@ -4,7 +4,26 @@ import "./TopBar.css";
 function TopBar() {
 
     const [toggledButtons, setToggledButtons] = useState({home:false, about:false, projects:false});
+
+    const [topName, setTopName] = useState('');
+    const nameineed = 'Noormunds Malnačs';
+    let i = 0;
   
+    const writeName = () => {
+      if (i < nameineed.length) {
+        setTopName(prevTopName => prevTopName + nameineed.charAt(i));
+        i++;
+      }else{
+        setTopName("Normunds Malnačs");
+      }
+    };
+  
+    useEffect(() => {
+      const intervalId = setInterval(writeName, 200);
+      return () => clearInterval(intervalId);
+    }, []);
+
+
     const changeToggleButtons = (button) => {
       switch (button) {
         case "home":
@@ -24,7 +43,7 @@ function TopBar() {
   
     const NavigatorButtons = ()=> {
       return (
-        <span className='mx-6 md:mx-3 lg:mg-6 xl:mx-9 select-none text-lg md:text-lg lg:text-xl xl:text-2xl'>
+        <span className='mx-1 md:mx-3 lg:mg-6 xl:mx-9 select-none text-lg md:text-lg lg:text-xl xl:text-2xl'>
           <a href='#home' className='mx-2 sizeOnHover' onClick={()=>changeToggleButtons("home")} style={{borderBottom: toggledButtons.home ? "1px solid black" : ""}}>Home</a>
           <a href='#about' className='mx-2 sizeOnHover' onClick={()=>changeToggleButtons("about")} style={{borderBottom: toggledButtons.about ? "1px solid black" : ""}}>About</a>
           <a href='#projects' className='mx-2 sizeOnHover' onClick={()=>changeToggleButtons("projects")} style={{borderBottom: toggledButtons.projects ? "1px solid black" : ""}}>Projects</a>
@@ -35,6 +54,7 @@ function TopBar() {
 
 
   useEffect(() => {
+    writeName();
       const handleResize = () => {
           const currentHash = window.location.hash;
           const element = document.querySelector(currentHash);
@@ -48,6 +68,7 @@ function TopBar() {
       return () => {
           window.removeEventListener('resize', handleResize);
       };
+
     }, []);
 
 
@@ -55,7 +76,7 @@ function TopBar() {
   
     return (
       <div className='h-20 w-full rounded-lg flex justify-between items-center bg-my-light' >
-          <span className='mx-6 md:mx-3 lg:mg-6 xl:mx-6 text-2xl md:text-2xl lg:text-3xl xl:text-4xl coolFont'>Jāāānis Ceeeeplis</span>
+          <span className='mx-6 md:mx-3 lg:mg-6 xl:mx-6 text-2xl md:text-2xl lg:text-3xl xl:text-4xl coolFont'>{topName}</span>
           <NavigatorButtons></NavigatorButtons>
           <div className='hidden md:block text-lg md:text-lg lg:text-xl xl:text-2xl'>Nu kkas seit arii var but</div>
       </div>
